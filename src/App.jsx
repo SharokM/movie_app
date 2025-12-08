6efa41c73f19b6251cb951492da3f27074011726
sharok_m <sharok.d.mcdonnaugh@gmail.com>
2025-12-08 17:12:28 +0000

    Fix folder structure

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <h1>Movie App</h1>
+    <p>lets make a movie site</p>      
+    </>
+  )
+}
+
+export default App
